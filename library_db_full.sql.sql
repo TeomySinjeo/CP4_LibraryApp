3ebe8c4244914111a2c89c4e5ec61c380a662293
Teomy <teomy.sinjeo@gmail.com>
2026-04-08 13:10:05 +0300

    Update №1 library_db_full.sql.sql
    Fixing problems with importing BD

--- library_db_full.sql.sql
+++ library_db_full.sql.sql
@@ -1,9 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: librarydb
--- ------------------------------------------------------
--- Server version	8.0.45
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -15,13 +9,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `authors`
---
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE DATABASE IF NOT EXISTS `librarydb`;
+USE `librarydb`;
 
 DROP TABLE IF EXISTS `authors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `authors` (
   `author_id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
@@ -29,25 +22,13 @@ CREATE TABLE `authors` (
   `birth_year` smallint DEFAULT NULL,
   PRIMARY KEY (`author_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `authors`
---
-
-LOCK TABLES `authors` WRITE;
-/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors` VALUES (1,'Лев','Толстой',1828),(2,'Фёдор','Достоевский',1821),(3,'Джордж','Оруэлл',1903);
-/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `books`
---
+INSERT INTO `authors` (`author_id`, `first_name`, `last_name`, `birth_year`) VALUES
+(1, 'Лев', 'Толстой', 1828),
+(2, 'Фёдор', 'Достоевский', 1821),
+(3, 'Джордж', 'Оруэлл', 1903);
 
 DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `books` (
   `book_id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
@@ -59,25 +40,32 @@ CREATE TABLE `books` (
   KEY `author_id` (`author_id`),
   CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`) ON DELETE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `books`
---
+INSERT INTO `books` (`book_id`, `title`, `author_id`, `genre`, `total_copies`, `available_copies`) VALUES
+(1, 'Война и мир', 1, 'Роман', 3, 3),
+(2, 'Анна Каренина', 1, 'Роман', 2, 2),
+(3, 'Преступление и наказание', 2, 'Психологический роман', 4, 4),
+(4, '1984', 3, 'Антиутопия', 5, 5),
+(5, 'Скотный двор', 3, 'Сатира', 2, 2);
 
-LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'Война и мир',1,'Роман',3,3),(2,'Анна Каренина',1,'Роман',2,2),(3,'Преступление и наказание',2,'Психологический роман',4,4),(4,'1984',3,'Антиутопия',5,5),(5,'Скотный двор',3,'Сатира',2,2);
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `readers`;
+CREATE TABLE `readers` (
+  `reader_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `registration_date` date DEFAULT (curdate()),
+  PRIMARY KEY (`reader_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `loans`
---
+INSERT INTO `readers` (`reader_id`, `first_name`, `last_name`, `email`, `phone`, `registration_date`) VALUES
+(1, 'Ростислав', 'Гаврилов', 'r.gavrilov@mail.ru', '89111111111', '2026-03-28'),
+(2, 'Антон', 'Богатов', 'a.bogatov@mail.ru', '89222222222', '2026-03-28'),
+(3, 'Ростислав', 'Гаврилов', 'r.gavrilov1@mail.ru', '83333333333', '2026-03-28');
 
 DROP TABLE IF EXISTS `loans`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `loans` (
   `loan_id` int NOT NULL AUTO_INCREMENT,
   `reader_id` int NOT NULL,
@@ -91,48 +79,14 @@ CREATE TABLE `loans` (
   CONSTRAINT `loans_ibfk_1` FOREIGN KEY (`reader_id`) REFERENCES `readers` (`reader_id`) ON DELETE CASCADE,
   CONSTRAINT `loans_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `loans`
---
+INSERT INTO `loans` (`loan_id`, `reader_id`, `book_id`, `loan_date`, `due_date`, `return_date`) VALUES
+(2, 1, 1, '2026-03-28', '2026-04-11', NULL),
+(3, 1, 2, '2026-03-28', '2026-04-11', '2026-03-28');
 
-LOCK TABLES `loans` WRITE;
-/*!40000 ALTER TABLE `loans` DISABLE KEYS */;
-INSERT INTO `loans` VALUES (2,1,1,'2026-03-28','2026-04-11',NULL),(3,1,2,'2026-03-28','2026-04-11','2026-03-28');
-/*!40000 ALTER TABLE `loans` ENABLE KEYS */;
-UNLOCK TABLES;
+SET FOREIGN_KEY_CHECKS = 1;
 
---
--- Table structure for table `readers`
---
-
-DROP TABLE IF EXISTS `readers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `readers` (
-  `reader_id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `registration_date` date DEFAULT (curdate()),
-  PRIMARY KEY (`reader_id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `readers`
---
-
-LOCK TABLES `readers` WRITE;
-/*!40000 ALTER TABLE `readers` DISABLE KEYS */;
-INSERT INTO `readers` VALUES (1,'Ростислав','Гаврилов','r.gavrilov@mail.ru','89111111111','2026-03-28'),(2,'Антон','Богатов','a.bogatov@mail.ru','89222222222','2026-03-28'),(3,'Ростислав','Гаврилов','r.gavrilov1@mail.ru','83333333333','2026-03-28');
-/*!40000 ALTER TABLE `readers` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -140,5 +94,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-03-28 12:38:15
